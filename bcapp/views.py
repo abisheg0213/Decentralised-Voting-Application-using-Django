53cb598168b6b571b10a22e7ec6b9a896e39d232
abisheg0213 <109604992+abisheg0213@users.noreply.github.com>
2023-03-22 14:45:11 +0530

    template updated

--- bcapp/views.py
+++ bcapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import web3data
+import web3.exceptions as t
 
 web3data.call_me_first()
 # Create your views here.
@@ -11,28 +12,40 @@ def dishome(request):
 
 
 def dis1(request):
-
-    return render(request, 'register.html')
+    if(web3data.view_stage()==1):
+        return render(request, 'register.html')
+    else:
+        message = "Time to register New voter has been completed"
+        return render(request, 'alert.html', {'text': message})
 def reg_user(request):
     address=request.POST['address']
     web3data.register_voter(address)
-    message="successfully registered"
-    return render(request,'display.html',{'text':message})
+    data="successfully registered"
+    print(data)
+    return render(request,'display.html',{'data':data})
 
 
 def disresult(request):
-    y=web3data.win_proposal()
-    d={0:'MONK',1:'DMK',2:'BJP'}
-    return render(request, 'result.html',{'data':d[y]})
+    if (web3data.view_stage() != 3):
+        return render(request,'alert.html')
+    else:
+        y=web3data.win_proposal()
+        d={0:'MONK',1:'DMK',2:'BJP'}
+        print(d[y])
+        return render(request, 'result.html',{'data':d[y]})
 def votecand(request):
-    if request.method=='POST':
-        a=request.POST['address']
-        b=int(request.POST['candidate'])
-        c = request.POST['private key']
-        web3data.vote(a,b,c)
+    if (web3data.view_stage() != 2):
+        return render(request, 'alert1.html')
+    else:
+        if request.method=='POST':
+            a=request.POST['address']
+            b=int(request.POST['candidate'])
+            c = request.POST['private key']
+            web3data.vote(a,b,c)
+            s="sucessfully voted"
+            return render(request,'display.html',{'text':s})
         s="Successfully Voted"
-        return render(request,'display.html',{'text':s})
-    return render(request, 'vote.html')
+        return render(request, 'vote.html')
 def change_state(request):
     if request.method=='POST':
         s=request.POST['state']
--- bcapp/web3data.py
+++ bcapp/web3data.py
@@ -1,6 +1,6 @@
 from solcx import compile_source, install_solc
 from web3 import Web3
-install_solc()
+# install_solc()
 con_instance=""
 w3=Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 ballot=""
@@ -12,117 +12,113 @@ def compile():
     compiled_sol = compile_source(
         '''
         pragma solidity ^0.8.14;
-        contract ballot
+contract ballot
+{
+    struct voter
+    {
+        uint weight;
+        bool voted;
+    }
+    struct candidate
+    {
+        uint votecount;
+        // string canname;
+    }
+    enum Stage{init,reg,vote,done}
+    Stage public stage=Stage.init;
+    address chairperson;
+    mapping (address => voter) Voter;
+    candidate [3] cans;
+    address [] voters;
+
+    constructor()
+    {
+        chairperson=msg.sender;
+        stage=Stage.reg;
+        // cans=new candidate[](noofcandidates);
+        Voter[msg.sender].weight=2;
+        Voter[msg.sender].voted=false;
+        voters.push(chairperson);
+      
+    }
+    function change_state(uint k) public onlyby(msg.sender){
+        if (k==2)
         {
-            struct voter
+            stage=Stage.vote;
+        }
+        if(k==3)
+        {
+            stage=Stage.done;
+        }
+    }
+
+    modifier onlyby(address s)
+    {
+        require(s==chairperson);
+        _;
+    }
+    modifier vervoted(address h)
+    {
+        require(Voter[h].voted==false);
+        _;
+    }
+    modifier reqstage(Stage t)
+    {
+        require(stage==t);
+        _;
+    }
+    function register(address vt) public onlyby(msg.sender) reqstage(Stage.reg)
+    {
+        Voter[vt].weight=1;
+        Voter[vt].voted=false;
+        voters.push(vt);
+       
+    }
+    modifier bvalvoter(address h)
+    {
+        require(regvoter(h)==true);
+        _;
+    }
+    function regvoter(address r) public view returns(bool)
+    {
+        uint y=0;
+        bool avail;
+        for(uint i=0;i<voters.length;i++)
+        {
+            // v=i;
+            if(voters[i]==r)
             {
-                uint weight;
-                bool voted;
-            }
-            struct candidate
-            {
-                uint votecount;
-                // string canname;
-            }
-            enum Stage{init,reg,vote,done}
-            Stage public stage=Stage.init;
-            address chairperson;
-            mapping (address => voter) Voter;
-            candidate [3] cans;
-            address [] voters;
-        
-            constructor()
-            {
-                chairperson=msg.sender;
-                stage=Stage.reg;
-                // cans=new candidate[](noofcandidates);
-                Voter[msg.sender].weight=2;
-                Voter[msg.sender].voted=false;
-                voters.push(chairperson);
-        
-            }
-            function change_state(uint k) public onlyby(msg.sender){
-                if (k==1)
-                {
-                    stage=Stage.reg;
-                }
-                if (k==2)
-                {
-                    stage=Stage.vote;
-                }
-                if(k==3)
-                {
-                    stage=Stage.done;
-                }
-            }
-        
-            modifier onlyby(address s)
-            {
-                require(s==chairperson);
-                _;
-            }
-            modifier vervoted(address h)
-            {
-                require(Voter[h].voted==false);
-                _;
-            }
-            modifier reqstage(Stage t)
-            {
-                require(stage==t);
-                _;
-            }
-            function register(address vt) public onlyby(msg.sender) reqstage(Stage.reg)
-            {
-                Voter[vt].weight=1;
-                Voter[vt].voted=false;
-                voters.push(vt);
-        
-            }
-            modifier bvalvoter(address h)
-            {
-                require(regvoter(h)==true);
-                _;
-            }
-            function regvoter(address r) public returns(bool)
-            {
-                uint y=0;
-                bool avail;
-                for(uint i=0;i<voters.length;i++)
-                {
-                    // v=i;
-                    if(voters[i]==r)
-                    {
-                        avail=true;
-                        y=1;
-                    }
-                }
-                if (y==0)
-                {
-                    avail=false;
-                }
-                return avail;
-            }
-            function vote(uint8 i) public vervoted(msg.sender) reqstage(Stage.vote) bvalvoter(msg.sender)
-            {
-                   cans[i].votecount+=Voter[msg.sender].weight;
-                   Voter[msg.sender].voted=true;
-        
-            }
-            function winningProposal() view public reqstage(Stage.done) returns(uint8)
-            {
-                uint max=0;
-                uint8 winp=0;
-                for (uint8 j=0;j<cans.length;j++)
-                {
-                    if (max<cans[j].votecount)
-                    {
-                        max=cans[j].votecount;
-                        winp=j;
-                    }
-                }
-                return winp;
+                avail=true;
+                y=1;
             }
         }
+        if (y==0)
+        {
+            avail=false;
+        }
+        return avail;
+    }
+    function vote(uint8 i) public vervoted(msg.sender) bvalvoter(msg.sender)
+    {
+        require(stage==Stage.vote);
+        cans[i].votecount+=Voter[msg.sender].weight;
+        Voter[msg.sender].voted=true;
+    }
+    function winningProposal() view public reqstage(Stage.done) returns(uint8)
+    {
+        uint max=0;
+        uint8 winp=0;
+        for (uint8 j=0;j<cans.length;j++)
+        {
+            if (max<cans[j].votecount)
+            {
+                max=cans[j].votecount;
+                winp=j;
+            }
+        }
+        return winp;
+    }
+}
         ''',
         output_values=['abi', 'bin']
     )
